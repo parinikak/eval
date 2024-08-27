6fa2b34e9a1be25bf2918835c4c2ce8818813fee
parinikak <22103094@mail.jiit.ac.in>
2024-08-27 16:10:06 +0530

    Create doc.py

---
+++ doc.py
@@ -0,0 +1,24 @@
+#1
+import numpy as np
+user_input = input("Enter a list of characters or integers separated by space: ").split()
+frequency = {}
+for item in user_input:
+    if(item.isUpper):
+        print(f(frequency.get(item,0))
+    else:
+        print((f, "SMALLER"(frequency.get(item,0))
+    #frequency[item] = frequency.get(item, 0) + 1
+
+print("Frequency of each element:", frequency)
+        
+#2
+import os
+user_input = input("Enter a list of characters or integers separated by space: ").split()
+frequency = {}
+for item in user_input:
+    frequency[item] = frequency.get(item, 0) + 1
+
+print("Frequency of each element:", frequency)
+        
+
+
